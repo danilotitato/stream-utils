c57987e76f78dd733e7c6d898140aabb47fd9c8b
Danilo Titato <danilotitato@gmail.com>
2021-10-26 19:04:56 -0300

    Set first endpoint

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.HTTP_PORT || 3000;
+
+app.use('/api', require('./route/route.js'));
+
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+})
---
+++ route/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/cotdtime', (req, res) => {
+});
+
+module.exports = router;
