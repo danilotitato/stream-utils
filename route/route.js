10105c1f6daa5f50f7668075deaf9b5f4e0dcaa6
Danilo Titato <danilotitato@gmail.com>
2021-10-27 20:20:54 -0300

    Add cotd result retriever

--- route/route.js
+++ route/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const retrievePlayerId = require('../utils/retrieve-player-id.js');
 const cotdRemainingTime = require('../utils/cotd-remaining-time.js');
+const lastCotdResult = require('../utils/last-cotd-result.js');
 const isInputValidString = require('../utils/validate-input.js').isInputValidString;
 
 router.get('/', (req, res) => {
@@ -11,13 +13,33 @@ router.get('/cotdtime', (req, res) => {
     res.send(cotdRemainingTime());
 });
 
-router.get('/lastcotd', (req, res) => {
-    const accountId = req.query.query;
+router.get('/lastcotd', async (req, res) => {
+    const name = req.query.name;
 
-    if (!isInputValidString(accountId)) {
-        console.log("lastcotd: Invalid account id");
-        res.status(400).send("Invalid account id");
+    if (!isInputValidString(name)) {
+        console.log(`lastcotd: Invalid player name ${name}`);
+        res.status(400).send('Invalid player name');
         return;
+    }
+
+    try {
+        const accountId = await retrievePlayerId(name);
+
+        if (!accountId) {
+            res.status(404).send('Player not found');
+            return;
+        }
+
+        const result = await lastCotdResult(accountId);
+
+        if (!result) {
+            res.status(500).send('Malformed response from trackmania.io');
+            return;
+        }
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 });
 
---
+++ utils/last-cotd-result.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const { DateTime } = require('luxon');
+
+const lastCotdResult = async accountId => {
+    const cupNumberName = {
+        '1': 'Cup of the Day',
+        '2': 'Cup of the Night',
+        '3': 'Cup of the Morning',
+    }
+
+    try {
+        const { data } = await axios.get(`https://trackmania.io/api/player/${accountId}/cotd/0`, {
+            headers: { 'User-Agent': 'tm-stream-utils/0.1.0 +https://github.com/danilotitato/TM-stream-utils' }
+        });
+
+        const lastCotdData = data.cotds[0];
+ 
+        const fullName = lastCotdData.name;
+        const cupNumber = fullName.split('#')[1];
+ 
+        const cupName = cupNumberName[cupNumber]
+ 
+        if (!cupName) {
+            console.error('Error: ', 'Invalid cup number');
+            return;
+        }
+
+        const cupDate = DateTime.fromISO(lastCotdData.timestamp).toLocaleString({ month: 'long', day: 'numeric', year: 'numeric' });
+ 
+        const percentageRank = ((lastCotdData.rank / lastCotdData.totalplayers) * 100).toFixed(2);
+ 
+        return `${cupName} of ${cupDate} results: Division ${lastCotdData.div}, Rank ${lastCotdData.divrank}. Overall rank: ${lastCotdData.rank} (top ${percentageRank}%)`;
+    } catch (error) {
+        console.error('Error: ', error.message);
+        throw error;
+    }
+}
+
+module.exports = lastCotdResult;
