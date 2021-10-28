f2260dd55364f8e4931c0476a4c1820b02655dc0
Danilo Titato <danilotitato@gmail.com>
2021-10-27 22:12:40 -0300

    Added matchmaking rank retrieval (3v3 and Royal)

--- route/route.js
+++ route/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const retrievePlayerId = require('../utils/retrieve-player-id.js');
 const cotdRemainingTime = require('../utils/cotd-remaining-time.js');
 const lastCotdResult = require('../utils/last-cotd-result.js');
+const rankMatchMaking = require('../utils/rank-matchmaking.js');
 const isInputValidString = require('../utils/validate-input.js').isInputValidString;
 
 router.get('/', (req, res) => {
@@ -17,7 +18,6 @@ router.get('/lastcotd', async (req, res) => {
     const name = req.query.name;
 
     if (!isInputValidString(name)) {
-        console.log(`lastcotd: Invalid player name ${name}`);
         res.status(400).send('Invalid player name');
         return;
     }
@@ -31,6 +31,64 @@ router.get('/lastcotd', async (req, res) => {
         }
 
         const result = await lastCotdResult(accountId);
+
+        if (!result) {
+            res.status(500).send('Malformed response from trackmania.io');
+            return;
+        }
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get('/3v3rank', async (req, res) => {
+    const name = req.query.name;
+
+    if (!isInputValidString(name)) {
+        res.status(400).send('Invalid player name');
+        return;
+    }
+
+    try {
+        const accountId = await retrievePlayerId(name);
+
+        if (!accountId) {
+            res.status(404).send('Player not found');
+            return;
+        }
+
+        const result = await rankMatchMaking(accountId, '3v3');
+
+        if (!result) {
+            res.status(500).send('Malformed response from trackmania.io');
+            return;
+        }
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get('/royalrank', async (req, res) => {
+    const name = req.query.name;
+
+    if (!isInputValidString(name)) {
+        res.status(400).send('Invalid player name');
+        return;
+    }
+
+    try {
+        const accountId = await retrievePlayerId(name);
+
+        if (!accountId) {
+            res.status(404).send('Player not found');
+            return;
+        }
+
+        const result = await rankMatchMaking(accountId, 'Royal');
 
         if (!result) {
             res.status(500).send('Malformed response from trackmania.io');
