fe3759d77423898f8896d0229cfe4f97116cd51e
Danilo Titato <danilotitato@gmail.com>
2021-11-12 17:38:50 -0300

    Remove date from COTD ranking info (message was too long)

--- utils/cotd-ranking.js
+++ utils/cotd-ranking.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const { DateTime } = require('luxon');
 
 const cotdRanking = async (accountId, isPrimaryOnly, isAvgOnly) => {
     try {
@@ -16,27 +15,18 @@ const cotdRanking = async (accountId, isPrimaryOnly, isAvgOnly) => {
         }
 
         const bestOverallRank = isPrimaryOnly ? data.stats.bestprimary.bestrank : data.stats.bestoverall.bestrank;
-        const bestOverallRankTime = DateTime
-            .fromISO(isPrimaryOnly ? data.stats.bestprimary.bestranktime : data.stats.bestoverall.bestranktime)
-            .toLocaleString({ month: 'long', day: 'numeric', year: 'numeric' });
 
         const bestDiv = isPrimaryOnly ? data.stats.bestprimary.bestdiv : data.stats.bestoverall.bestdiv;
         const bestRankDivRank = isPrimaryOnly ? data.stats.bestprimary.bestrankdivrank : data.stats.bestoverall.bestrankdivrank;
-        const bestDivTime = DateTime
-            .fromISO(isPrimaryOnly ? data.stats.bestprimary.bestdivtime : data.stats.bestoverall.bestdivtime)
-            .toLocaleString({ month: 'long', day: 'numeric', year: 'numeric' });
 
         const bestRankInDiv = isPrimaryOnly ? data.stats.bestprimary.bestrankindiv : data.stats.bestoverall.bestrankindiv;
         const bestRankInDivDiv = isPrimaryOnly ? data.stats.bestprimary.bestrankindivdiv : data.stats.bestoverall.bestrankindivdiv;
-        const bestRankInDivTime = DateTime
-            .fromISO(isPrimaryOnly ? data.stats.bestprimary.bestrankindivtime : data.stats.bestoverall.bestrankindivtime)
-            .toLocaleString({ month: 'long', day: 'numeric', year: 'numeric' });
 
         return isAvgOnly
             ? `top ${avgRank}%, being in division ${avgDiv} in average.`
-            : `best overall rank has been ${bestOverallRank} on ${bestOverallRankTime}. ` +
-                `The best division has been ${bestDiv}, finished in rank ${bestRankDivRank}, on ${bestDivTime}. ` +
-                `And the best rank in a cup has been ${bestRankInDiv} in division ${bestRankInDivDiv} on ${bestRankInDivTime}`;
+            : `best overall rank: ${bestOverallRank}. ` +
+                `Best division: ${bestDiv} (rank ${bestRankDivRank}). ` +
+                `Best rank in a cup: ${bestRankInDiv} in div ${bestRankInDivDiv}`;
     } catch (error) {
         console.error('Error: ', error.message);
         throw error;
