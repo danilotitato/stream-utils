29e80e130386a14a8ba2f9517ce38a889638c64e
Danilo Titato <danilotitato@gmail.com>
2021-12-29 16:31:09 -0300

    Use scrapeapi proxy to scrape tiktok page

--- socials/tiktok.js
+++ socials/tiktok.js
@@ -1,13 +1,27 @@
+require('dotenv').config();
 const axios = require('axios');
+
+const timer = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const lastTiktokPost = async username => {
     try {
-        const videoUrlRe = /https:\/\/www.tiktok.com\/@\w+\/video\/\d+/gm;
+        const videoUrlRe = /www.tiktok.com\/@\w+\/video\/\d+/gm;
 
-        const page = await axios.get(`www.tiktok.com/@${username}`, {cache: {maxAge: 60 * 60 * 1000}}, {
-            headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' }});
+        let lastVideos;
+        while(true) {
+            const tiktokUrl = `www.tiktok.com/@${username}`;
+            const scraperUrl = `http://api.scraperapi.com?api_key=${process.env.SCRAPERAPI_KEY}&url=${tiktokUrl}`;
+            const page = await axios.get(scraperUrl, {cache: {maxAge: 60 * 60 * 1000}}, {
+                headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' }});
 
-        return page.data.match(videoUrlRe)[0];
+            lastVideos = page.data.match(videoUrlRe);
+
+            if (lastVideos) {
+                return lastVideos[0];
+            }
+
+            await timer(500);
+        }
     } catch (error) {
         console.error('Error: ', error.message);
         throw error;
