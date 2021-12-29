ccc6ac350fcf25b99344c635311b3b7f96604799
Danilo Titato <danilotitato@gmail.com>
2021-12-29 16:03:04 -0300

    Add 1 hour cache setting to tiktok requisition

--- socials/tiktok.js
+++ socials/tiktok.js
@@ -4,7 +4,7 @@ const lastTiktokPost = async username => {
     try {
         const videoUrlRe = /https:\/\/www.tiktok.com\/@\w+\/video\/\d+/gm;
 
-        const page = await axios.get(`www.tiktok.com/@${username}`, {
+        const page = await axios.get(`www.tiktok.com/@${username}`, {cache: {maxAge: 60 * 60 * 1000}}, {
             headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36' }});
 
         return page.data.match(videoUrlRe)[0];
