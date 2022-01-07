776bcbc35177ce55203f361a338f41fc3412a18f
Danilo Titato <danilotitato@gmail.com>
2022-01-06 22:16:13 -0300

    Move maps out of the method

--- trackmania/rank-matchmaking.js
+++ trackmania/rank-matchmaking.js
@@ -1,36 +1,37 @@
 const axios = require('axios');
 
+const matchMakingType = {
+    '3v3': 0,
+    'Royal': 1,
+};
+
+const rankName3v3 = {
+    '1': 'Bronze I',
+    '2': 'Bronze II',
+    '3': 'Bronze III',
+    '4': 'Silver I',
+    '5': 'Silver II',
+    '6': 'Silver III',
+    '7': 'Gold I',
+    '8': 'Gold II',
+    '9': 'Gold III',
+    '10': 'Master I',
+    '11': 'Master II',
+    '12': 'Master III',
+    '13': 'Trackmaster',
+};
+
+const rankNameRoyal = {
+    '1': 'Unranked',
+    '2': 'Bronze',
+    '3': 'Silver',
+    '4': 'Gold',
+    '5': 'Master',
+};
+
+const rankNames = [rankName3v3, rankNameRoyal];
+
 const rankMatchMaking = async (accountId, type) => {
-    const matchMakingType = {
-        '3v3': 0,
-        'Royal': 1,
-    };
-
-    const rankName3v3 = {
-        '1': 'Bronze I',
-        '2': 'Bronze II',
-        '3': 'Bronze III',
-        '4': 'Silver I',
-        '5': 'Silver II',
-        '6': 'Silver III',
-        '7': 'Gold I',
-        '8': 'Gold II',
-        '9': 'Gold III',
-        '10': 'Master I',
-        '11': 'Master II',
-        '12': 'Master III',
-        '13': 'Trackmaster',
-    };
-
-    const rankNameRoyal = {
-        '1': 'Unranked',
-        '2': 'Bronze',
-        '3': 'Silver',
-        '4': 'Gold',
-        '5': 'Master',
-    };
-
-    const rankNames = [rankName3v3, rankNameRoyal];
 
     try {
         const { data } = await axios.get(`https://trackmania.io/api/player/${accountId}`, {
