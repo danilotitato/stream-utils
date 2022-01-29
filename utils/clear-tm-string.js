65d675772778b5f138007fe70aa5108107397909
Danilo Titato <danilotitato@gmail.com>
2022-01-29 12:36:36 -0300

    Include uppercase formatters in TMString regex

--- utils/clear-tm-string.js
+++ utils/clear-tm-string.js
@@ -1,5 +1,5 @@
 const clearTMString = input => {
-    const re = /\$([0-9A-Fa-f]{1,3}|[wnoitsgz]{1})/g;
+    const re = /\$([0-9A-Fa-f]{1,3}|[wnoitsgzWNOITSGZ]{1})/g;
     return input.replaceAll(re, '');
 }
 
