84c1d574f331c7a6f46787834fea0dd829851d6b
Danilo Titato <danilotitato@gmail.com>
2022-03-25 02:33:22 -0300

    Add random youtube video getter

---
+++ services/youtube/random-video.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const ytch = require('yt-channel-info');
+const getRandomElement = require('../../utils/random-element.js');
+
+const randomVideo = async channel => {
+    try {
+        const firstPayload = {
+            channelId: channel,
+            channelIdType: 3
+         }
+
+        const firstResponse = await ytch.getChannelVideos(firstPayload);
+        const videoList = [];
+
+        if (!firstResponse.alertMessage) {
+            videoList.push(...firstResponse.items);
+
+            let continuation = firstResponse.continuation;
+
+            while (continuation) {
+                const nextPayload = {
+                    continuation: continuation
+                }
+
+                const nextResponse = await ytch.getChannelVideosMore(nextPayload);
+
+                videoList.push(...nextResponse.items);
+
+                continuation = nextResponse.continuation;
+            }
+
+            const randomVideoItem = getRandomElement(videoList);
+            const randomVideoTitle = randomVideoItem.title
+            const randomVideoId = randomVideoItem.videoId
+
+            return `${randomVideoTitle} - https://youtu.be/${randomVideoId}`;
+         } else {
+            console.error('Channel could not be found.')
+            throw firstResponse.alertMessage;
+         }
+    } catch (error) {
+        console.error('Error: ', error.message);
+        throw error;
+    }
+}
+
+module.exports = randomVideo;
