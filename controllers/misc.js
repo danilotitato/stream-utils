fef02ba8781a3c60258bddecaadd1456c0cea247
Danilo Titato <danilotitato@gmail.com>
2022-04-01 11:02:01 -0300

    Add special char converter

--- controllers/misc.js
+++ controllers/misc.js
@@ -3,6 +3,7 @@ const req = require('express/lib/request');
 const miscRouter = express.Router();
 
 const convertRank = require('../services/misc/convert-rank.js');
+const regularizeChars = require('../services/misc/regularize-chars.js');
 
 const isInputValidString = require('../utils/validate-input.js').isInputValidString;
 
@@ -23,6 +24,28 @@ miscRouter.get('/convertrank', async (req, res) => {
         }
 
         res.send(result);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+miscRouter.get('/regularchars', async (req, res) => {
+    const str = req.query.str;
+
+    if (!isInputValidString(str)) {
+        res.status(400).send('Invalid input');
+        return;
+    }
+
+    try {
+        const result = regularizeChars(str);
+
+        if (!result) {
+            res.status(500).send('Error converting');
+            return;
+        }
+
+        res.send(encodeURIComponent(result));
     } catch (error) {
         res.status(400).send(error.message);
     }
